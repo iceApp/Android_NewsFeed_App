dda0fc8a6e34c8439dcf431361de9582e256849a
iceApp <worktest2000@gmail.com>
2020-06-05 16:38:47 +0900

    First Comitt

---
+++ app/src/main/java/com/example/newsfeed/ui/MainFragment.kt
@@ -0,0 +1,29 @@
+package com.example.newsfeed.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.newsfeed.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/newsfeed/viewmodel/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.newsfeed.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(app: Application) : AndroidViewModel(app) {
+
+}
